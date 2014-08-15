9b90c9c006df27e348f52447bd917fbe4c972cb1
Gary Smith <scopevale@gmail.com>
2014-08-15 02:44:45 +0000

    added newrelic tracking

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // Orchestrates the loading of Ghost
 // When run from command line.
 
+// New Relic app monitoring
+require('newrelic');
+
 var ghost = require('./core'),
     errors = require('./core/server/errors');
 
---
+++ newrelic.js
@@ -0,0 +1,25 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['We The People - Ghost blog on AWS'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '74060c81de50fb2a44c6b8f5b4e4886c988442b0',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
+
